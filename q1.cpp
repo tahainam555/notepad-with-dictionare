0fbb853b1adc9281e06f286ac476f091e87bfda8
tahainam555 <tahainam879@gmail.com>
2024-11-09 18:38:17 +0500

    insertion

--- q1.cpp
+++ q1.cpp
@@ -1,6 +1,50 @@
 #include<iostream>
 using namespace std;
 
+class Node{
+    public:
+        string data;
+        Node* left;
+        Node* right;
+        int height;
+
+        Node(string data){
+            this->data = data;
+            left = NULL;
+            right = NULL;
+            height = 0;
+        }
+
+};
+
+class AVL{
+    public:
+        Node* root;
+        AVL(){
+            root = NULL;
+        }
+        void insert(string data){
+            root = insert(root, data);
+        }
+        Node* insert(Node* root, string data){
+            if(root == NULL){
+                return new Node(data);
+            }
+            if(data < root->data){
+                root->left = insert(root->left, data);
+            }
+            else if(data > root->data){
+                root->right = insert(root->right, data);
+            }
+        }
+
+        Node* search(string data){
+            
+        }
+
+
+};
+
+
 int main(){
-    
 }
